4e564ab70b0113bacfa6c1a7e72e88a0df705aa4
Abeer-Ragab <123848391+Abeer-M-Ali@users.noreply.github.com>
2024-10-04 04:36:17 +0300

    2-main.js

--- 0x02-ES6_classes/tests/2-main.js
+++ 0x02-ES6_classes/tests/2-main.js
@@ -1,43 +1,18 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-      throw new Error('Invalid input type');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
+import HolbertonCourse from './2-hbtn_course';
 
-  get name() {
-    return this._name;
-  }
+const c1 = new HolbertonCourse('ES6', 1, ['Bob', 'Jane']);
+console.log(c1.name);
+c1.name = 'Python 101';
+console.log(c1);
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new Error('Invalid input type');
-    }
-    this._name = value;
-  }
+try {
+  c1.name = 12;
+} catch (err) {
+  console.log(err);
+}
 
-  get length() {
-    return this._length;
-  }
-
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new Error('Invalid input type');
-    }
-    this._length = value;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  set students(value) {
-    if (!Array.isArray(value)) {
-      throw new Error('Invalid input type');
-    }
-    this._students = value;
-  }
+try {
+  const c2 = new HolbertonCourse('ES6', '1', ['Bob', 'Jane']);
+} catch (err) {
+  console.log(err);
 }
